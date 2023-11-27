f4ae2d12103d3397aa437a8ca77dba278e363b6c
nurulloh <nurullomahmud@gmail.com>
2023-11-27 18:42:32 +0500

    working on navbar

--- root/urls.py
+++ root/urls.py
@@ -1,10 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+
+def landing_page(request):
+    return render(request, 'base.html')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='home'),
     path('users/', include('users.urls')),
     path('brokers/', include('brokers.urls')),
     path('invoices/', include('invoices.urls')),
